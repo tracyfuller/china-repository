1282db6d49f5332d5c20be1a0d1f57de71eb96c5
tracyfuller <tracy.e.fuller@gmail.com>
2021-06-14 14:50:39 -0500

    testing up a db table

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM items');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
